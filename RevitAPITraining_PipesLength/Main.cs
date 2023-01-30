47d4247b6f784be54c1e35f65e4dfd39817beb9e
Dmitriy Baranov <bdvbuild@gmail.com>
2023-01-30 14:09:52 +0400

    Add project files.

---
+++ RevitAPITraining_PipesLength/Main.cs
@@ -0,0 +1,40 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace RevitAPITraining_PipesLength
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            IList<Reference> selectedRefList = uidoc.Selection.PickObjects(ObjectType.Element, new PipesFilter(), "Выберите трубы");
+            double lengthSum = 0;
+
+            foreach (Reference selectedElem in selectedRefList)
+            {
+                Pipe pipe = doc.GetElement(selectedElem) as Pipe;
+                Parameter pipeLength = pipe.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH);
+                var length = UnitUtils.ConvertFromInternalUnits(pipeLength.AsDouble(), UnitTypeId.Meters);
+                lengthSum+= length;
+            }
+
+            TaskDialog.Show("Трубопровод", $"Длина труб: {lengthSum}м");
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ RevitAPITraining_PipesLength/PipesFilter.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI.Selection;
+using System;
+
+namespace RevitAPITraining_PipesLength
+{
+    internal class PipesFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            if (elem is Pipe)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+}
